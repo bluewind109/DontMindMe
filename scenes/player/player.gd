c0b68d54f57163a6225b124af91baae0ccdff934
Khanh Hoang Quoc <khanh.hoangquoc91@gmail.com>
2024-05-08 14:57:29 +0700

    Add player scene

---
+++ scenes/player/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const SPEED: float = 200.0
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	rotation = velocity.angle()
+
+
+func get_input() -> void:
+	var new_velocity = Vector2.ZERO
+	new_velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	new_velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	velocity = new_velocity.normalized() * SPEED
+
+
