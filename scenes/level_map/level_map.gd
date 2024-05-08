e85cbcebcd2c03f98fa098e265a3ad59518e13bc
Khanh Hoang Quoc <khanh.hoangquoc91@gmail.com>
2024-05-08 17:08:14 +0700

    Add exit scene

--- scenes/level_map/level_map.gd
+++ scenes/level_map/level_map.gd
@@ -8,6 +8,7 @@ var _collected: int = 0
 func _ready():
 	_pickups_count = pick_ups.get_children().size()
 	SignalManager.on_pickup.connect(on_pickup)
+	SignalManager.on_exit.connect(on_exit)
 
 
 func _process(delta):
@@ -24,3 +25,7 @@ func on_pickup() -> void:
 	print("on_pickup")
 	_collected += 1
 	check_show_exit()
+
+
+func on_exit() -> void:
+	print("GAME OVER, WIN")
--- singletons/signal_manager.gd
+++ singletons/signal_manager.gd
@@ -2,3 +2,4 @@ extends Node
 
 signal on_pickup
 signal on_show_exit
+signal on_exit
